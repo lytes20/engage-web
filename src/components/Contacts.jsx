18275a22503d4a5f9ca1cf7a5e72cf246baf6a0a
Gideon Bamuleseyo <gideonbamuleseyo@gmail.com>
2020-03-10 15:45:53 +0300

    Separate main contacts table from side

--- src/components/Contacts.jsx
+++ src/components/Contacts.jsx
@@ -1,12 +1,15 @@
 import React from "react";
+import Button from "@material-ui/core/Button";
+
 import "../assets/styles/contacts.scss";
 
 import ContactsTable from "./ContactsTable";
+
 function Contacts() {
   return (
     <div className="contacts-container">
       {/* Contacts main container  */}
-      <div>
+      <div className="contacts-main">
         <div>
           <h1>Contacts</h1>
         </div>
@@ -18,7 +21,21 @@ function Contacts() {
       </div>
 
       {/* Contacts Side bar  */}
-      <div></div>
+      <div className="contacts-side">
+        <div>
+          import
+          <Button variant="contained" color="secondary">
+            Actions
+          </Button>
+          <Button variant="contained" color="secondary">
+            Create New
+          </Button>
+        </div>
+        <div>
+          All Contacts All Saved Filters Meet all of the following conditions
+          Add Filter
+        </div>
+      </div>
     </div>
   );
 }
--- src/components/ContactsTable.jsx
+++ src/components/ContactsTable.jsx
@@ -4,33 +4,47 @@ import Table from "@material-ui/core/Table";
 import TableBody from "@material-ui/core/TableBody";
 import TableCell from "@material-ui/core/TableCell";
 import TableRow from "@material-ui/core/TableRow";
+import TableContainer from "@material-ui/core/TableContainer";
+import Paper from '@material-ui/core/Paper';
+import { withStyles } from "@material-ui/core/styles";
 
 import CustomTableHead from "./CustomTableHead";
 
-import { contacts } from '../data'
+import { contacts } from "../data";
 
+export const CustomTableRow = withStyles({
+  root: {
+    cursor: "pointer"
+  }
+})(TableRow);
 function ContactsTable() {
   const [order, setOrder] = React.useState("asc");
   const [orderBy, setOrderBy] = React.useState("id");
 
   return (
-    <Table aria-label="custom pagination table">
-      <CustomTableHead
-        order={order}
-        orderBy={orderBy}
-        rowCount={contacts.length}
-      />
-      <TableBody>
-        {contacts.map(contact => (
-          <TableRow key={contact.name} onClick={() => console.log(contact)}>
-            <TableCell>{contact.name}</TableCell>
-            <TableCell>{contact.date}</TableCell>
-            <TableCell>{contact.location}</TableCell>
-            <TableCell>{contact.status}</TableCell>
-          </TableRow>
-        ))}
-      </TableBody>
-    </Table>
+    <TableContainer component={Paper}>
+      <Table aria-label="custom pagination table">
+        <CustomTableHead
+          order={order}
+          orderBy={orderBy}
+          rowCount={contacts.length}
+        />
+        <TableBody>
+          {contacts.map(contact => (
+            <CustomTableRow
+              hover
+              key={contact.name}
+              onClick={() => console.log(contact)}
+            >
+              <TableCell>{contact.name}</TableCell>
+              <TableCell>{contact.date}</TableCell>
+              <TableCell>{contact.location}</TableCell>
+              <TableCell>{contact.status}</TableCell>
+            </CustomTableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
   );
 }
 
