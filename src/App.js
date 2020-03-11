c922348255671599ebad0e123f059833693e757b
Gideon Bamuleseyo <gideonbamuleseyo@gmail.com>
2020-03-11 12:27:09 +0300

    Start style contacts side

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import NavBar from './components/NavBar'
-import Contacts from './components/Contacts'
-import Routes from './routes'
+import React from "react";
+import NavBar from "./components/NavBar";
+import Contacts from "./components/Contacts";
+import Routes from "./routes";
 function App() {
   return (
-    <div>
+    <div style={{ height: "100%" }}>
       <NavBar />
       <Routes />
     </div>
--- src/components/Contacts.jsx
+++ src/components/Contacts.jsx
@@ -1,17 +1,40 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import Button from "@material-ui/core/Button";
+import { withStyles } from "@material-ui/core";
 
 import "../assets/styles/contacts.scss";
 
 import ContactsTable from "./ContactsTable";
+import { DropDown, MenuDropDown } from "./DropDown";
+
+export const CreateNewButton = withStyles({
+  root: {
+    cursor: "pointer",
+    backgroundColor: "#FD7556",
+    color: "#fff",
+    "&.MuiButton-root": {
+      backgroundColor: "#FD7556"
+    },
+    "& .item-label": {
+      color: "#fff",
+      textTransform: "capitalize"
+    }
+  }
+})(Button);
 
 function Contacts() {
   return (
     <div className="contacts-container">
       {/* Contacts main container  */}
       <div className="contacts-main">
-        <div>
+        <div className="contacts-heading contacts-head-item">
           <h1>Contacts</h1>
+          <div className="sort-by-container">
+            <p>Sort By</p>
+            Created Date
+            {/* <MenuDropDown item="Created Date" /> */}
+          </div>
         </div>
 
         {/* Contacts table  */}
@@ -22,18 +45,17 @@ function Contacts() {
 
       {/* Contacts Side bar  */}
       <div className="contacts-side">
-        <div>
-          import
-          <Button variant="contained" color="secondary">
-            Actions
-          </Button>
-          <Button variant="contained" color="secondary">
-            Create New
-          </Button>
+        <div className="contacts-head-item">
+          <Link className="contacts-side-item">Import</Link>
+          <Button className="contacts-side-item">Actions</Button>
+          <CreateNewButton className="contacts-side-item">
+            <span className="item-label">Create New</span>
+          </CreateNewButton>
         </div>
-        <div>
-          All Contacts All Saved Filters Meet all of the following conditions
-          Add Filter
+        <div className="contacts-side-actions-container">
+          <Button style={{ backgroundColor: "#ECEDF1", width:"100%", textAlign: "right", textTransform:"capitalize"}}>All Contacts</Button>
+          <Button>All Saved</Button>
+          Meet all of the following conditions Add Filter
         </div>
       </div>
     </div>
