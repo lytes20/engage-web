8774f4818a0cc77ddaa429380de2478e26149e5c
Gideon Bamuleseyo <gideonbamuleseyo@gmail.com>
2020-03-12 17:48:19 +0300

    Add dummy data back

---
+++ src/data/index.js
@@ -0,0 +1,10 @@
+function createTab(name) {
+  return { name };
+}
+export const tabsItems = [
+  createTab("Activities"),
+  createTab("Emails"),
+  createTab("Deals"),
+  createTab("Tasks"),
+  createTab("Notes")
+];
